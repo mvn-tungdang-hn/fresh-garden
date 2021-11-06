7314d9390ee580dbf1bcaec3fc849186f3d6df1d
mvn-tungdang-hn <tung.dang@monstar-lab.com>
2021-11-06 22:03:52 +0700

    update db

--- database/fresh_garden.sql
+++ database/fresh_garden.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 12, 2021 at 12:10 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Generation Time: Nov 06, 2021 at 04:03 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,10 +44,8 @@ CREATE TABLE `banners` (
 --
 
 INSERT INTO `banners` (`id`, `title`, `thumbnail`, `link`, `display_order`, `is_external`, `status`, `created_at`, `updated_at`) VALUES
-(1, '', 'public/images/upload/banner/banner1.jpg', '', 1, 0, 1, '2021-09-03 08:43:00', '2021-09-03 08:43:00'),
-(2, NULL, 'public/images/upload/banner/banner2.jpg', NULL, 2, 1, 1, '2021-09-03 08:43:17', '2021-09-03 08:43:17'),
-(3, NULL, 'public/images/upload/banner/banner3.jpg', NULL, 3, 1, 1, '2021-09-03 08:43:27', '2021-09-03 08:43:27'),
-(6, 'Banner 1', 'public/images/upload/banner/1631351840thumbnail.png', '', 1, 0, 0, '2021-09-11 09:17:20', '2021-09-11 09:17:20');
+(9, 'Abc', 'public/images/upload/banner/1636184219home1-slide1.jpg', '', 1, 0, 1, '2021-11-06 07:36:59', '2021-11-06 07:36:59'),
+(10, 'Xyz', 'public/images/upload/banner/1636184231home1-slide2.jpg', '', 2, 0, 1, '2021-11-06 07:37:11', '2021-11-06 07:37:11');
 
 -- --------------------------------------------------------
 
@@ -72,14 +70,8 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `title`, `thumbnail`, `parent_id`, `type`, `status`, `display_order`, `created_at`, `updated_at`) VALUES
-(1, 'Bánh Trung thu', 'public/images/upload/category/1631349401thumbnail.png', 10, 1, 0, 1, '2021-09-03 08:26:54', '2021-09-03 08:26:54'),
-(2, 'Bánh mì tươi', 'public/images/upload/category/banhtrungthu.jpg', 1, 1, 1, 2, '2021-09-03 08:35:18', '2021-09-03 08:35:18'),
-(3, 'Bánh kem tươi', 'public/images/upload/category/banhtrungthu.jpg', NULL, 1, 1, 3, '2021-09-03 08:35:41', '2021-09-03 08:35:41'),
-(4, 'Bánh pizza', 'public/images/upload/category/banhtrungthu.jpg', NULL, 1, 1, 4, '2021-09-03 08:36:05', '2021-09-03 08:36:05'),
-(10, 'Bánh mì chảo', 'public/images/upload/category/1631349959thumbnail.png', 1, 1, 0, 1, '2021-09-11 08:45:59', '2021-09-11 08:45:59'),
-(11, 'Bánh hoa quả', 'public/images/upload/category/1631434102download (1).png', 10, 1, 1, 12, '2021-09-12 08:08:22', '2021-09-12 08:08:22'),
-(12, 'Form title 123', NULL, 2, 1, 1, 12, '2021-09-12 08:10:18', '2021-09-12 08:10:18'),
-(16, 'Form title 123', NULL, NULL, 1, 1, 1, '2021-09-12 08:19:39', '2021-09-12 08:19:39');
+(21, 'Bánh mì tươi', NULL, NULL, 1, 1, 1, '2021-11-06 07:37:39', '2021-11-06 07:37:39'),
+(22, 'Bánh kem tươi', NULL, NULL, 1, 1, 2, '2021-11-06 07:37:55', '2021-11-06 07:37:55');
 
 -- --------------------------------------------------------
 
@@ -136,10 +128,21 @@ CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `total_money` int(11) NOT NULL,
-  `total_quatity` int(11) NOT NULL,
+  `total_quantity` int(11) NOT NULL,
+  `payment_method_id` int(11) DEFAULT NULL,
+  `shipping_method_id` int(11) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `total_money`, `total_quantity`, `payment_method_id`, `shipping_method_id`, `status`, `created_at`, `updated_at`) VALUES
+(6, 1, 75000, 3, 1, 1, 3, '2021-11-06 09:09:03', '2021-11-06 09:09:03'),
+(7, 1, 75000, 3, 1, 1, 1, '2021-11-06 14:54:06', '2021-11-06 14:54:06');
 
 -- --------------------------------------------------------
 
@@ -156,6 +159,35 @@ CREATE TABLE `order_details` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `price`, `quantity`, `created_at`, `updated_at`) VALUES
+(5, 6, 11, 25000, 3, '2021-11-06 09:09:03', '2021-11-06 09:09:03'),
+(6, 7, 11, 25000, 3, '2021-11-06 14:54:06', '2021-11-06 14:54:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_methods`
+--
+
+CREATE TABLE `payment_methods` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payment_methods`
+--
+
+INSERT INTO `payment_methods` (`id`, `title`, `created_at`, `updated_at`) VALUES
+(1, 'Thanh toán tiền mặt khi nhận hàng', '2021-11-06 07:31:33', '2021-11-06 07:31:33'),
+(2, 'Thanh toán bằng chuyển khoản', '2021-11-06 07:31:33', '2021-11-06 07:31:33');
 
 -- --------------------------------------------------------
 
@@ -187,9 +219,10 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `title`, `original_price`, `price`, `description`, `content`, `include`, `thumbnail`, `status`, `quantity`, `is_hot`, `views`, `category_id`, `collection_id`, `created_at`, `updated_at`) VALUES
-(1, 'Hộp 4 bánh (Dòng truyền thống)', 350000, 200000, 'Dòng bánh Fresh Garden lan tỏa giá trị truyền thống, giá trị nguyên bản và tích cực về sống chậm, được cảm nhận rõ qua nét thi vị của mùa trăng hiện đại với sự đa dạng sắc vị thơm ngon, vừa quen vừa lạ như nhân đậu xanh táo đỏ, nhân đậu đỏ, nhân sữa dừa, nhân khoai môn, nhân hạt sen mè đen, nhân hạt sen hạt dưa, nhân đậu xanh 1 trứng, nhân thập cẩm 1 trứng', 'Dòng bánh Fresh Garden lan tỏa giá trị truyền thống, giá trị nguyên bản và tích cực về sống chậm, được cảm nhận rõ qua nét thi vị của mùa trăng hiện đại với sự đa dạng sắc vị thơm ngon, vừa quen vừa lạ như nhân đậu xanh táo đỏ, nhân đậu đỏ, nhân sữa dừa, nhân khoai môn, nhân hạt sen mè đen, nhân hạt sen hạt dưa, nhân đậu xanh 1 trứng, nhân thập cẩm 1 trứng', 'Hộp gồm 4 bánh nhân tuỳ chọn truyền thống.', 'public/images/upload/product/product1.jpg', 1, 10, 1, NULL, 1, 1, '2021-09-03 09:06:07', '2021-09-03 09:06:07'),
-(2, 'Hộp 6 bánh Thu hoa viên (Dòng cao cấp)', 350000, 200000, '<p>Dòng bánh Fresh Garden lan tỏa giá trị truyền thống, giá trị nguyên bản và tích cực về sống chậm, được cảm nhận rõ qua nét thi vị của mùa trăng hiện đại với sự đa dạng sắc vị thơm ngon, vừa quen vừa lạ như nhân đậu xanh táo đỏ, nhân đậu đỏ, nhân sữa dừa, nhân khoai môn, nhân hạt sen mè đen, nhân hạt sen hạt dưa, nhân đậu xanh 1 trứng, nhân thập cẩm 1 trứng</p>', '<p>Dòng bánh Fresh Garden lan tỏa giá trị truyền thống, giá trị nguyên bản và tích cực về sống chậm, được cảm nhận rõ qua nét thi vị của mùa trăng hiện đại với sự đa dạng sắc vị thơm ngon, vừa quen vừa lạ như nhân đậu xanh táo đỏ, nhân đậu đỏ, nhân sữa dừa, nhân khoai môn, nhân hạt sen mè đen, nhân hạt sen hạt dưa, nhân đậu xanh 1 trứng, nhân thập cẩm 1 trứng</p>', '            Hộp gồm 6 bánh nhân tùy chọn dòng cao cấp, có túi đi kèm          ', 'public/images/upload/product/product1.jpg', 1, 10, 1, NULL, 1, 1, '2021-09-03 09:06:07', '2021-09-03 09:06:07'),
-(5, 'Bánh trứng', 100000, 80000, '<p>1234</p>', '<p>1234</p>', '1234', 'public/images/upload/product/1631359855thumbnail.png', 1, 10, 1, NULL, 1, 1, '2021-09-11 11:30:55', '2021-09-11 11:30:55');
+(11, 'Bánh tươi xúc xích phô mai', 30000, 25000, 'Bánh tươi sử dụng nguyên liệu cao cấp. Chiếc bánh hình tam giác được kẹp xúc xích hòa quyện lớp phô mai tươi', '<p>Nội dung….</p>', 'Bột bánh burger (bột mỳ, men, đường, muối, trứng, bơ, nước), xúc xích (thịt gà & thịt lợn),phô mai, sốt bơ tỏi, sốt Mayonnaise, bột bánh gối', 'public/images/upload/product/1636184347hummingbird-printed-t-shirt.jpg', 1, 5, 1, NULL, 21, 1, '2021-11-06 07:39:07', '2021-11-06 07:39:07'),
+(12, 'Bánh tươi xúc xích', 0, 20000, 'Bánh tươi sử dụng nguyên liệu cao cấp. Vỏ bánh mềm mịn cuộn lớp xúc xích và phomai tươi.\r\n', '<p>Nội dung…</p>', 'Bột bánh mỳ tươi (bột mỳ, đường, men, sữa bột, trứng gà, muối, bơ, nước), xúc xích (thịt gà & thịt lợn), phomai, hành lá', 'public/images/upload/product/1636184393the-adventure-begins-framed-poster.jpg', 1, 11, 0, NULL, 21, 2, '2021-11-06 07:39:53', '2021-11-06 07:39:53'),
+(13, 'Bánh tươi nhân kem vani', 50000, 35000, 'Bánh tươi sử dụng nguyên liệu cao cấp. Mặt bánh phủ xúc xích và ngô hạt\r\n', '<p>Bánh tươi sử dụng nguyên liệu cao cấp. Mặt bánh phủ xúc xích và ngô hạt</p>', 'Bột bánh mỳ tươi (bột mỳ, đường, men, sữa bột, trứng gà, muối, bơ, nước) phomai, xúc xích, ngô hạt, sốt Mayonnaise                      ', 'public/images/upload/product/1636184470the-best-is-yet-to-come-framed-poster.jpg', 1, 100, 1, NULL, 21, 3, '2021-11-06 07:41:10', '2021-11-06 07:41:10'),
+(14, 'Bánh tươi cuộn xúc xích', 44000, 34000, 'Bánh tươi sử dụng nguyên liệu cao cấp. Vỏ bánh mềm mịn cuộn xúc xích đặc biêt.\r\n', '<p>Bánh tươi sử dụng nguyên liệu cao cấp. Vỏ bánh mềm mịn cuộn xúc xích đặc biêt.</p>', 'Bột bánh mỳ tươi (bột mỳ, đường, men, sữa bột, trứng gà, muối, bơ, nước), xúc xích, bột ớt không cay                      ', 'public/images/upload/product/1636184510today-is-a-good-day-framed-poster.jpg', 1, 100, 1, NULL, 21, 4, '2021-11-06 07:41:50', '2021-11-06 07:41:50');
 
 -- --------------------------------------------------------
 
@@ -205,14 +238,27 @@ CREATE TABLE `product_images` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `product_images`
+-- Table structure for table `shipping_methods`
 --
 
-INSERT INTO `product_images` (`id`, `product_id`, `thumbnail`, `created_at`, `updated_at`) VALUES
-(1, 1, 'http://localhost/fresh-garden/public/images/upload/product-image/product-image-1.jfif', '2021-09-03 10:15:40', '2021-09-03 10:15:40'),
-(2, 1, 'http://localhost/fresh-garden/public/images/upload/product-image/product-image-2.jfif', '2021-09-03 10:15:40', '2021-09-03 10:15:40'),
-(3, 1, 'http://localhost/fresh-garden/public/images/upload/product-image/product-image-3.jfif', '2021-09-03 10:15:47', '2021-09-03 10:15:47');
+CREATE TABLE `shipping_methods` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shipping_methods`
+--
+
+INSERT INTO `shipping_methods` (`id`, `title`, `price`, `created_at`, `updated_at`) VALUES
+(1, 'Giao tiêu chuẩn (3 - 5 ngày)', 30000, '2021-11-06 07:32:28', '2021-11-06 07:32:28'),
+(2, 'Hoả tốc (giao ngay trong ngày)', 50000, '2021-11-06 07:32:28', '2021-11-06 07:32:28');
 
 -- --------------------------------------------------------
 
@@ -274,7 +320,9 @@ ALTER TABLE `news`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id_fk` (`user_id`);
+  ADD KEY `user_id_fk` (`user_id`),
+  ADD KEY `payment_method_id_fk` (`payment_method_id`),
+  ADD KEY `shipping_method_id_fk` (`shipping_method_id`);
 
 --
 -- Indexes for table `order_details`
@@ -283,6 +331,12 @@ ALTER TABLE `order_details`
   ADD PRIMARY KEY (`id`),
   ADD KEY `order_id_fk` (`order_id`),
   ADD KEY `product_id_fkk` (`product_id`);
+
+--
+-- Indexes for table `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `products`
@@ -300,6 +354,12 @@ ALTER TABLE `product_images`
   ADD KEY `product_id_fk` (`product_id`);
 
 --
+-- Indexes for table `shipping_methods`
+--
+ALTER TABLE `shipping_methods`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -313,13 +373,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `banners`
 --
 ALTER TABLE `banners`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `collections`
@@ -337,19 +397,25 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `product_images`
@@ -358,10 +424,16 @@ ALTER TABLE `product_images`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `shipping_methods`
+--
+ALTER TABLE `shipping_methods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -371,6 +443,8 @@ ALTER TABLE `users`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
+  ADD CONSTRAINT `payment_method_id_fk` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_methods` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `shipping_method_id_fk` FOREIGN KEY (`shipping_method_id`) REFERENCES `shipping_methods` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
